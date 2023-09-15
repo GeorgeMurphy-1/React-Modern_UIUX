829a0ee666b1a381a56b76d24e9d5e6c00f0ace4
GeorgeMurphy-1 <0GeorgeMurphy@gmail.com>
2023-09-15 15:34:04 -0400

    initial commit of source code for modern React App.

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Footer, Blog, Possibility, WhatisGPT4, Header, Features} from "./containers/main";
+import { CTA, Brand, Navbar} from "./components/main"
+import "./App.css"
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+        <Brand />
+        <WhatisGPT4 />
+        <Features />
+        <Possibility />
+        <CTA  />
+        <Blog />
+        <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/cta/CTA.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './cta.css';
+
+const CTA = () => {
+  return (
+    <div>
+      CTA
+    </div>
+  )
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export default CTA
